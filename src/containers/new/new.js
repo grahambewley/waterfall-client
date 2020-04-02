87da95ff1c4d4fdd9c579c04b64081e8a75dc3de
Graham Bewley <grahambewley@gmail.com>
2020-04-01 22:56:58 -0400

    Create new game form

--- src/containers/new/new.js
+++ src/containers/new/new.js
@@ -4,28 +4,90 @@ import baseUrl from '../../utils/baseUrl';
 import axios from 'axios';
 
 const New = () => {
-    // const [gameId, setGameId] = React.useState();
+    const [formDisabled, setFormDisabled] = React.useState(false);
+    const [gameData, setGameData] = React.useState();
+    const [password, setPassword] = React.useState(); // Stored separately so it can be used to join the game after creating
 
     async function createGame(e) {
         e.preventDefault();
-        // Create game and get back game ID
-        const url = `${baseUrl}/createGame`;
-        const payload = {
-            gameName: e.target.elements.gameName.value
-        };
-        console.log("gameName to send is " + e.target.elements.gameName.value);
-        const response = await axios.post(url, payload);
-        console.log(response);
+        
+        setFormDisabled(true);
+        
+        const gameName = e.target.elements.gameName.value;
+        const password = e.target.elements.gamePassword.value;
+        setPassword(password);
+
+        try {
+            // Create game and get back game ID
+            const url = `${baseUrl}/createGame`;
+            const payload = {
+                gameName,
+                password
+            };
+            const response = await axios.post(url, payload);
+            setGameData(response.data.gameData);
+            console.log(response);
+        } catch(error) {
+            alert("Sorry, there was an error starting your game: ", error);
+        } finally {
+            setFormDisabled(false);
+        }
+    }
+
+    async function joinGame(e) {
+        e.preventDefault();
+        
+        setFormDisabled(true);
+        
+        const playerName = e.target.elements.playerName.value;
+        
+        try {
+            // Join game and get back status (either response or error)
+            const url = `${baseUrl}/joinGame`;
+            const payload = {
+                shortId: gameData.shortId,
+                password,
+                playerName
+            };
+            const response = await axios.post(url, payload);
+            console.log(response);
+        } catch(error) {
+            alert("Sorry, there was an error joining the game: ", error);
+        } finally {
+            setFormDisabled(false);
+        }
     }
 
     return (
         <div className={classes.wrapper}>
             <h1 className={classes.header}>Start New Game</h1>
 
-            <form className={classes.form} onSubmit={createGame}>
-                <label htmlFor='gameName'>Game Name</label>
-                <input name='gameName' type='text' placeholder='i.e. Happy Hour Club'/>   
-            </form>
+            { !gameData ? 
+                <form disabled={formDisabled} className={classes.form} onSubmit={createGame}>
+                    <div className={classes.formItem}>
+                        <label className={classes.formLabel} htmlFor='gameName'>Game Name</label>
+                        <input className={classes.formInput} name='gameName' type='text' placeholder='i.e. Happy Hour Club'/>
+                    </div>
+                    <div className={classes.formItem}>
+                        <label className={classes.formLabel} htmlFor='gamePassword'>Password (required to enter game)</label>
+                        <input className={classes.formInput} name='gamePassword' type='password'/>
+                    </div>
+                    <div className={classes.formItem}>
+                        <input className={classes.formButton} type='submit' value='Create'/>
+                    </div>
+                </form>
+            :
+                <form disabled={formDisabled} className={classes.form} onSubmit={joinGame}>
+                    <div className={classes.formItem}>
+                        <label className={classes.formLabel} htmlFor='playerName'>Your Display Name</label>
+                        <input className={classes.formInput} name='playerName' type='text' placeholder='i.e John'/>
+                    </div>
+                    
+                    <div className={classes.formItem}>
+                        <input className={classes.formButton} type='submit' value='Start'/>
+                    </div>
+                </form>
+            }
         </div>
     )
 }
--- src/containers/start/start.js
+++ src/containers/start/start.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import classes from './start.module.scss';
+
 
 const Start = () => {
     return(<>
@@ -7,9 +9,12 @@ const Start = () => {
             <h1 className={ classes.header }>Waterfall</h1>
             <h2 className={ classes.subhead }>The Drinking Game: Online</h2>
 
-            <button className={ classes.joinButton }>Join Game</button>
-            <button className={ classes.startButton }>Start New Game</button>
-
+            <Link to="/join">
+                <button className={ classes.joinButton }>Join Game</button>
+            </Link>
+            <Link to="/new">
+                <button className={ classes.startButton }>Start New Game</button>
+            </Link>
             <span role='img' aria-label='beer glasses' style={{fontSize: '6rem'}}>&#127867;</span>
         </div>
     </>)
