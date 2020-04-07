0669ff3f1fdd35b5c285a0df78d339f9ff2aa03b
Graham Bewley <grahambewley@gmail.com>
2020-04-07 08:19:28 -0400

    Tip on game modes

--- src/containers/new/new.js
+++ src/containers/new/new.js
@@ -5,6 +5,7 @@ import baseUrl from '../../utils/baseUrl';
 import axios from 'axios';
 import randomString from 'randomstring';
 import cookie from 'js-cookie';
+import Tip from '../../components/tip/tip';
 
 const New = () => {
     const [formDisabled, setFormDisabled] = React.useState(false);
@@ -88,11 +89,59 @@ const New = () => {
                         <input className={classes.formInput} name='gameName' type='text' placeholder='e.g. Happy Hour Club'/>
                     </div>
                     <div className={classes.formItem}>
-                        <label className={classes.formLabel} htmlFor='gamePassword'>Game Rules</label>
-                        <select name='gameMode' className={classes.formSelect}>
-                            <option className={classes.formOption} value='classic'>Classic</option>
-                            <option className={classes.formOption} value='simplified'>Simplified</option>
-                        </select>
+                        <label className={classes.formLabel} htmlFor='gameMode'>Game Rules</label>
+                        <div className={classes.formItemWithTipWrapper}>
+                            <select name='gameMode' className={classes.formSelect}>
+                                <option className={classes.formOption} value='classic'>Classic</option>
+                                <option className={classes.formOption} value='simplified'>Simplified</option>
+                            </select>
+                            <Tip>
+                                <h2>Classic Rules:</h2>
+                                <p><strong>2 - 3:</strong>
+                                    <br/>Red = take this number of drinks
+                                    <br/>Black = give this number of drinks (can be split between players)</p>
+                                <p><strong>4:</strong> Ladies drink</p>
+                                <p><strong>5:</strong> Social, everyone drinks</p>
+                                <p><strong>6:</strong> Guys drink</p>
+                                <p><strong>7: Heaven</strong>
+                                <br/>Last to point to the sky drinks (video chat recommended!)</p>
+                                <p><strong>8: Date</strong>
+                                    <br/>This player picks a date. When this player has to drink, so does their date</p>
+                                <p><strong>9: Rhyme Time</strong>
+                                    <br/>This player picks a word to rhyme with. Each player, in order, shouts a word that rhymes with it. First player to fail to come up with a rhyming word must drink</p>
+                                <p><strong>10: Categories</strong>
+                                    <br/>This player picks a category (e.g. soda brands, US Presidents, etc.). Each player, in order, names something that fits the category. First player to fail to come up with one must drink</p>
+                                <p><strong>Jack: Thumb Master</strong>
+                                    <br/>This player is now Thumb Master. Whenever they put their thumb on the table (or in the air on video chat), all others must do the same. Last one to do so must drink</p>
+                                <p><strong>Queen: Question Master</strong>
+                                    <br/>This player is now Question Master. If they ask a question and another player answers, that player must drink</p>
+                                <p><strong>King: Rule Master</strong>
+                                    <br/>This player makes a new rule that others must follow for the rest of the game (e.g. no touching your face, etc). If a player breaks this rule, they must drink</p>
+                                <p><strong>Ace: Waterfall!</strong>
+                                    <br/>All players begin to drink. When the player who started the waterfall chooses to stop, the player after them can stop, and so on. The last player in line must finish last.</p>
+
+                                    <h2>Simplified Rules:</h2>
+                                <p><strong>2 - 6:</strong>
+                                    <br/>Red = take this number of drinks
+                                    <br/>Black = give this number of drinks (can be split between players)</p>
+                                <p><strong>7: Heaven</strong>
+                                <br/>Last to point to the sky drinks (video chat recommended!)</p>
+                                <p><strong>8: Date</strong>
+                                    <br/>This player picks a date. When this player has to drink, so does their date</p>
+                                <p><strong>9: Rhyme Time</strong>
+                                    <br/>This player picks a word to rhyme with. Each player, in order, shouts a word that rhymes with it. First player to fail to come up with a rhyming word must drink</p>
+                                <p><strong>10: Categories</strong>
+                                    <br/>This player picks a category (e.g. soda brands, US Presidents, etc.). Each player, in order, names something that fits the category. First player to fail to come up with one must drink</p>
+                                <p><strong>Jack: Thumb Master</strong>
+                                    <br/>This player is now Thumb Master. Whenever they put their thumb on the table (or in the air on video chat), all others must do the same. Last one to do so must drink</p>
+                                <p><strong>Queen: Question Master</strong>
+                                    <br/>This player is now Question Master. If they ask a question and another player answers, that player must drink</p>
+                                    <p><strong>King: Rule Master</strong>
+                                    <br/>This player makes a new rule that others must follow for the rest of the game (e.g. no touching your face, etc). If a player breaks this rule, they must drink</p>
+                                <p><strong>Ace: Waterfall!</strong>
+                                    <br/>All players begin to drink. When the player who started the waterfall chooses to stop, the player after them can stop, and so on. The last player in line must finish last.</p>
+                            </Tip>
+                        </div>
                     </div>
                     <div className={classes.formItem}>
                         <label className={classes.formLabel} htmlFor='gamePassword'>Game Password</label>
